f5d15aef2f830b81e4b6dcfc5cc96cae8958b4c0
strong-code <clindsay107@gmail.com>
2016-10-27 14:39:46 -0700

    add media controller and route

---
+++ app/controllers/media_controller.rb
@@ -0,0 +1,29 @@
+class MediaController < ApplicationController
+
+  def create
+    ext       = File.extname(params[:media].original_filename)
+    filename  = SecureRandom.hex[0..5] + ext
+    folder    = get_folder(ext)
+    directory = "public/media/#{folder}"
+    path = File.join(directory, filename)
+    File.open(path, 'wb') { |f| f.write(params[:media].read) }
+    render text: "http://strongco.de/media/#{folder}/#{filename}\n"
+  end
+
+  private
+
+  def get_folder(ext)
+    types = {
+      p: ['.png', '.jpg', '.gif', '.jpeg', '.tiff'],
+      v: ['.mpg', '.mp4', '.webm', '.avi', '.mkv', '.gifv', '.m4v'],
+      txt:   ['.txt', '.md', '.rtf', '.doc', '.docx']
+    }
+
+    types.each do |k, v|
+      return k.to_s if v.include?(ext)
+    end
+
+    'o'
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'home#index'
   resources :journal_entries
+  resource :media, only: [:create]
 end
