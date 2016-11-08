276b438019e6899b94e2842ebb9b3548cf5020b7
strong-code <clindsay107@gmail.com>
2016-11-08 12:55:33 -0800

    add health page

---
+++ app/controllers/health_controller.rb
@@ -0,0 +1,16 @@
+class HealthController < ApplicationController
+  before_filter :set_cache_headers
+  
+  def index
+    @body = `echo q | htop -s PERCENT_MEM | aha --line-fix`
+  end
+
+  private
+
+  def set_cache_headers
+    response.headers["Cache-Control"] = "no-cache, no-store"
+    response.headers["Pragam"] = "no-cache"
+    response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'home#index'
   get 'today', to: 'today#show'
+  get 'health', to: 'health#index'
   resources :journal_entries
   resource :media, only: [:create, :destroy]
 end
