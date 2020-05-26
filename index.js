be6e3a767516c96af46f02840877f39f3361820c
strong-code <colin@strongco.de>
2020-05-25 23:19:18 -0700

    finsh filestore and get/set for test pastes

--- index.js
+++ index.js
@@ -3,17 +3,16 @@ const app = express()
 const port = 3000
 const paste = require('./lib/pasteHandler.js')
 
+// Statically host pastes in /d
+app.use('/d', express.static('d'))
+
 app.post('/paste', (req, res) => {
   return paste.handleUpload(req, res)
 })
 
-app.route('/paste/:paste_id')
-  .get((req, res) => {
-    // get
-  })
-  .delete((req, res) => {
-    // delete
-  })
+app.delete('/paste/:key', (req, res) => {
+  // delete
+})
 
 app.get('/', (req, res) => {
   res.send(req.hostname)
